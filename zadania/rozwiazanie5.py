c1f404f19cf05656f9800becb48e66cac9d72d17
krzysiek513 <krzysiek3155@gmail.com>
2017-05-15 20:06:24 +0200

    czesciowe rozwiazanie 5

--- zadania/rozwiazanie5.py
+++ zadania/rozwiazanie5.py
@@ -1,6 +1,6 @@
 # Napisac funkcje, ktora dostaje dwie nazwy miesiecy i sprawdza, ile miesiecy mija od pierwszego do drugiego z #nich.  
 # Jesli drugi jest wczesniejszy niz pierwszy, zakladamy, ze jest on w nastepnym roku.
-a={
+rok={
     "styczen":1,
     "luty":2,
     "marzec":3,
@@ -15,81 +15,17 @@ a={
     "grudzien":12,
     }
 bar2 = {"imie" : "jurek", "nazwisko" : "lepper"}
-print (bar2["imie"])
-def misiace(start, koniec):
 
+def miesiace(start, koniec):
+    a = 1
+    pocz = (rok[start])
+    kon = (rok[koniec])
+    for i in range(pocz, kon):
+	    a = a + 1
 
+    print(a)
 
-'''
-git = False
-while not git:
-	pierwsza = raw_input("podaj pierwsza liczbe od 1 do 12: ")
-	if len(pierwsza) < 3 and len(pierwsza) > 0:
-		#print len(pierwsza)
-		if len(pierwsza) == 2:
-			#print type(pierwsza)
-			if  pierwsza=="10":
-				git = True
-			elif  pierwsza=="11":
-				git = True
-			elif  pierwsza=="12":
-				git = True
-			else:
-				git = False
-			
-		else:
-			print ord(pierwsza[0]), ord("1")
-			if ord(pierwsza[0]) > ord("1") or ord(pierwsza[0]) <= ord("9"):
-				git = True
-git = False
-while not git:
-	druga = raw_input("podaj druga liczbe od 1 do 12: ")
-	if len(druga) < 3 and len(druga) > 0:
-		#print len(druga)
-		if len(druga) == 2:
-			#print type(pierwsza)
-			if  pierwsza=="10":
-				git = True
-			elif  pierwsza=="11":
-				git = True
-			elif  pierwsza=="12":
-				git = True
-			else:
-				git = False
-			
-		else:
-			print ord(druga[0]), ord("1")
-			if ord(druga[0]) > ord("1") or ord(druga[0]) <= ord("9"):
-				git = True
+pierwszy = input("Podaj 1 miesiac ")
+drugi = input("Podaj 2 miesiac ")
 
-'''
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+miesiace(pierwszy, drugi)
